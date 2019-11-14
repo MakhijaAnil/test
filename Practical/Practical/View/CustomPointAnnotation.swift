8a10697b6ad3700405846a644d668788fc558004
MakhijaAnil <57771467+MakhijaAnil@users.noreply.github.com>
2019-11-15 01:27:59 +0530

    first
    first commit

---
+++ Practical/Practical/View/CustomPointAnnotation.swift
@@ -0,0 +1,15 @@
+//
+//  CustomPointAnnotation.swift
+//  Practical
+//
+//  Created by Anil on 14/11/19.
+//  Copyright © 2019 Anil. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class CustomPointAnnotation: MKPointAnnotation {
+    var pinCustomImageName:String!
+
+}
